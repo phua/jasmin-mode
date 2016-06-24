55be2ee90a5ffa41eb36ef06fb6919be24e307e8
Peter Hua <phua@users.noreply.github.com>
2016-06-23 19:11:09 -0500

    first commit

---
+++ jasmin-autoloads.el
@@ -0,0 +1,15 @@
+;;; jasmin-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("jasmin.el") (22359 34374 40260 411000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jasmin-autoloads.el ends here
